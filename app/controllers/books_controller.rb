014eb389a6e35766e46ad9b83b0cd292cce666c8
d-hamasato <dafubangli@gmail.com>
2020-11-27 10:41:42 +0000

    [add] indexのビュー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,16 @@
 class BooksController < ApplicationController
   def top
   end
-  
+
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -10,4 +18,16 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
